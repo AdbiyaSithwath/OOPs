d7c657fc2e51e48fdf29b2ab92d12668da4591ee
AdbiyaSithwath <66269369+AdbiyaSithwath@users.noreply.github.com>
2020-09-20 18:40:03 +0530

    OOPs.cpp

---
+++ OOPs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+class Time
+{
+    private:
+        int seconds;
+        int hh,mm,ss;
+    public:
+        void getTime(void);
+        void convertIntoSeconds(void);
+        void displayTime(void);
+};
+void Time::getTime(void)
+{
+    cout << "Time:" << endl;
+    cout << "Hours?";          cin >> hh;
+    cout << "Min?";          cin >> mm;
+    cout << "Sec?";          cin >> ss;
+}
+void Time::convertIntoSeconds(void)
+{
+    seconds = hh*3600 + mm*60 + ss;
+}
+void Time::displayTime(void)
+{
+    cout << "The time is = " << setw(2) << setfill('0') << hh << ":"
+                             << setw(2) << setfill('0') << mm << ":"
+                             << setw(2) << setfill('0') << ss << endl;
+    cout << "Time in total seconds: " << seconds;
+}
+int main()
+{
+    Time T;
+    T.getTime();
+    T.convertIntoSeconds();
+    T.displayTime();
+    return 0;
+}
